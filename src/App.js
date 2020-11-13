6c58ab26c3224b8f6c1f5c82ef31eadad1131290
Te <sofrente@gmail.com>
2020-11-13 13:17:51 +0700

    fix(*): add new design

--- src/App.js
+++ src/App.js
@@ -1,160 +1,205 @@
-import './App.css';
+import "./App.css";
 
 const data = [
   {
-    city: 'Bangkok',
-    video: 'https://www.w3schools.com/howto/rain.mp4',
+    city: "Bangkok",
+    video: "",
     holiday: {
       mon: [
-        'https://ca.slack-edge.com/T07SJTGPR-U0A3LM5E2-289dfe1e55cc-512',
-        'https://ca.slack-edge.com/T07SJTGPR-U08G7756V-ef69c8ddfd91-512',
-        'https://ca.slack-edge.com/T07SJTGPR-UMVHC4XPW-eac5cdcfcdcd-512'
+        "https://ca.slack-edge.com/T07SJTGPR-U08G7756V-ef69c8ddfd91-512",
       ],
       tue: [
-        'https://ca.slack-edge.com/T07SJTGPR-ULR70D8GH-87e881223f02-512',
-        'https://ca.slack-edge.com/T07SJTGPR-U01BLP0CH5Y-8a41cad1be4b-512'
+        "https://ca.slack-edge.com/T07SJTGPR-ULR70D8GH-87e881223f02-512",
+        "https://ca.slack-edge.com/T07SJTGPR-U01BLP0CH5Y-8a41cad1be4b-512",
       ],
       wed: [],
-      thur: [],
-      fri: []
+      thu: [],
+      fri: [
+        "https://ca.slack-edge.com/T07SJTGPR-UMVHC4XPW-eac5cdcfcdcd-512",
+      ],
     },
     wfh: {
-      mon: [],
-      tue: [],
-      wed: [],
-      thur: [
-        'https://ca.slack-edge.com/T07SJTGPR-ULR70D8GH-87e881223f02-512',
-        'https://ca.slack-edge.com/T07SJTGPR-U01BLP0CH5Y-8a41cad1be4b-512'
+      mon: [
+        "https://ca.slack-edge.com/T07SJTGPR-U08G63PQC-06ac15cf64b1-512",
+        "https://ca.slack-edge.com/T07SJTGPR-U0A3LM5E2-289dfe1e55cc-512",
+        "https://ca.slack-edge.com/T07SJTGPR-UMVHC4XPW-eac5cdcfcdcd-512",
+        "https://ca.slack-edge.com/T07SJTGPR-ULR70D8GH-87e881223f02-512",
+        "https://ca.slack-edge.com/T07SJTGPR-U01BLP0CH5Y-8a41cad1be4b-512",
       ],
-      fri: []
-    }
-  }
-]
+      tue: [],
+      wed: [
+        "https://ca.slack-edge.com/T07SJTGPR-U08G63PQC-06ac15cf64b1-512",
+        "https://ca.slack-edge.com/T07SJTGPR-U0A3LM5E2-289dfe1e55cc-512",
+        "https://ca.slack-edge.com/T07SJTGPR-UMVHC4XPW-eac5cdcfcdcd-512",
+        "https://ca.slack-edge.com/T07SJTGPR-ULR70D8GH-87e881223f02-512",
+        "https://ca.slack-edge.com/T07SJTGPR-U01BLP0CH5Y-8a41cad1be4b-512",
+      ],
+      thu: [
+        "https://ca.slack-edge.com/T07SJTGPR-ULR70D8GH-87e881223f02-512",
+        "https://ca.slack-edge.com/T07SJTGPR-U01BLP0CH5Y-8a41cad1be4b-512",
+      ],
+      fri: [
+        "https://ca.slack-edge.com/T07SJTGPR-U08G63PQC-06ac15cf64b1-512",
+        "https://ca.slack-edge.com/T07SJTGPR-U0A3LM5E2-289dfe1e55cc-512",
+        "https://ca.slack-edge.com/T07SJTGPR-UMVHC4XPW-eac5cdcfcdcd-512",
+        "https://ca.slack-edge.com/T07SJTGPR-ULR70D8GH-87e881223f02-512",
+        "https://ca.slack-edge.com/T07SJTGPR-U01BLP0CH5Y-8a41cad1be4b-512",
+      ],
+    },
+  },
+];
 
 function Calenday(props) {
-  const date = new Date()
+  const date = new Date();
   return (
-    <div className='background'>
-      <video autoPlay muted loop>
-        <source src={props.video} type='video/mp4' />
-      </video>
-      <div className='overlay'>
-        <div className='content'>
-          <div className='header'>
-            <h1>{props.city}</h1>
-            <h4>{date.toLocaleTimeString()}</h4>
+    <div className="container">
+      <div className="panel-title">
+        <div className="pull-left">
+          <span>Who's Out?</span>
+          <h1>{props.city}</h1>
+        </div>
+        <div className="pull-right">☀️ {date.toLocaleTimeString()}</div>
+      </div>
+      <div className="panel-weekly">
+        <div className="col col-title">
+          <div className="row">
+            <span>🌴</span>
+            <h3>Holiday</h3>
           </div>
-          <table style={{ width: '80%'}}>
-            <tr style={{ height: 110 }}>
-              <th style={{ width: '15%'}}></th>
-              <th style={{ width: '15%'}}><h3>Monday</h3><h5>9.11.2020</h5></th>
-              <th style={{ width: '15%'}}><h3>Tuesday</h3><h5>10.11.2020</h5></th>
-              <th style={{ width: '15%'}}><h3>Wednesday</h3><h5>11.11.2020</h5></th>
-              <th style={{ width: '15%'}}><h3>Thursday</h3><h5>12.11.2020</h5></th>
-              <th style={{ width: '15%'}}><h3>Friday</h3><h5>13.11.2020</h5></th>
-            </tr>
-            <tr>
-              <td>
-                Holiday <i className="twa twa-2x twa-palm-tree"></i>
-              </td>
-              <td>
-                {
-                  props.holiday.mon.map((image) => (
-                    <div className='avatar' style={{ backgroundImage: `url("${image}")`}}>
-                    </div>
-                  ))
-                }
-              </td>
-              <td>
-                {
-                  props.holiday.tue.map((image) => (
-                    <div className='avatar' style={{ backgroundImage: `url("${image}")`}}>
-                    </div>
-                  ))
-                }
-              </td>
-              <td>
-                {
-                  props.holiday.wed.map((image) => (
-                    <div className='avatar' style={{ backgroundImage: `url("${image}")`}}>
-                    </div>
-                  ))
-                }
-              </td>
-              <td>
-                {
-                  props.holiday.thur.map((image) => (
-                    <div className='avatar' style={{ backgroundImage: `url("${image}")`}}>
-                    </div>
-                  ))
-                }
-              </td>
-              <td>
-                {
-                  props.holiday.fri.map((image) => (
-                    <div className='avatar' style={{ backgroundImage: `url("${image}")`}}>
-                    </div>
-                  ))
-                }
-              </td>
-            </tr>
-            <tr>
-              <td>
-                WFH <i className="twa twa-2x twa-house-with-garden"></i>
-              </td>
-              <td>
-                {
-                  props.wfh.mon.map((image) => (
-                    <div className='avatar' style={{ backgroundImage: `url("${image}")`}}>
-                    </div>
-                  ))
-                }
-              </td>
-              <td>
-                {
-                  props.wfh.tue.map((image) => (
-                    <div className='avatar' style={{ backgroundImage: `url("${image}")`}}>
-                    </div>
-                  ))
-                }
-              </td>
-              <td>
-                {
-                  props.wfh.wed.map((image) => (
-                    <div className='avatar' style={{ backgroundImage: `url("${image}")`}}>
-                    </div>
-                  ))
-                }
-              </td>
-              <td>
-                {
-                  props.wfh.thur.map((image) => (
-                    <div className='avatar' style={{ backgroundImage: `url("${image}")`}}>
-                    </div>
-                  ))
-                }
-              </td>
-              <td>
-                {
-                  props.wfh.fri.map((image) => (
-                    <div className='avatar' style={{ backgroundImage: `url("${image}")`}}>
-                    </div>
-                  ))
-                }
-              </td>
-            </tr>
-          </table>
+          <div className="row">
+            <span>🏠</span>
+            <h3>WFH</h3>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="row">
+            <span className="date">MON 9</span>
+            <div className="avatar-list">
+              {props.holiday.mon.map((image) => (
+                <div
+                  className="avatar"
+                  style={{ backgroundImage: `url("${image}")` }}
+                ></div>
+              ))}
+            </div>
+          </div>
+          <div className="row">
+            <div className="avatar-list">
+              {props.wfh.mon.map((image) => (
+                <div
+                  className="avatar"
+                  style={{ backgroundImage: `url("${image}")` }}
+                ></div>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <span className="date">TUE 10</span>
+          <div className="row">
+            <div className="avatar-list">
+              {props.holiday.tue.map((image) => (
+                <div
+                  className="avatar"
+                  style={{ backgroundImage: `url("${image}")` }}
+                ></div>
+              ))}
+            </div>
+          </div>
+          <div className="row">
+            <div className="avatar-list">
+              {props.wfh.tue.map((image) => (
+                <div
+                  className="avatar"
+                  style={{ backgroundImage: `url("${image}")` }}
+                ></div>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <span className="date">WED 11</span>
+          <div className="row">
+            <div className="avatar-list">
+              {props.holiday.wed.map((image) => (
+                <div
+                  className="avatar"
+                  style={{ backgroundImage: `url("${image}")` }}
+                ></div>
+              ))}
+            </div>
+          </div>
+          <div className="row">
+          <div className="avatar-list">
+            {props.wfh.wed.map((image) => (
+              <div
+                className="avatar"
+                style={{ backgroundImage: `url("${image}")` }}
+              ></div>
+            ))}
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <span className="date">THU 12</span>
+          <div className="row">
+            <div className="avatar-list">
+              {props.holiday.thu.map((image) => (
+                <div
+                  className="avatar"
+                  style={{ backgroundImage: `url("${image}")` }}
+                ></div>
+              ))}
+            </div>
+          </div>
+          <div className="row">
+          <div className="avatar-list">
+            {props.wfh.thu.map((image) => (
+              <div
+                className="avatar"
+                style={{ backgroundImage: `url("${image}")` }}
+              ></div>
+            ))}
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <span className="date">FRI 13</span>
+          <div className="row">
+            <div className="avatar-list">
+              {props.holiday.fri.map((image) => (
+                <div
+                  className="avatar"
+                  style={{ backgroundImage: `url("${image}")` }}
+                ></div>
+              ))}
+            </div>
+          </div>
+          <div className="row">
+          <div className="avatar-list">
+            {props.wfh.fri.map((image) => (
+              <div
+                className="avatar"
+                style={{ backgroundImage: `url("${image}")` }}
+              ></div>
+            ))}
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 function App() {
-  const date = new Date()
   return data.map((d) => {
-    return <Calenday {...d} />
-  })
+    return <Calenday {...d} />;
+  });
 }
 
 export default App;
